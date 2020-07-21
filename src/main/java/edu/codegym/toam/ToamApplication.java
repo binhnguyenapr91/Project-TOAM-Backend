8e88541bcffd8f247c415b5cdba29afac1a3777b
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-07-21 21:29:42 +0700

    init project with security

---
+++ src/main/java/edu/codegym/toam/ToamApplication.java
@@ -0,0 +1,13 @@
+package edu.codegym.toam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/codegym/toam/ToamApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.codegym.toam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
