0efa351ba2d507f9bdc7a92dc952246f471dbee4
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-07-22 12:48:12 +0700

    test login to get token ok

--- src/main/java/edu/codegym/toam/service/account/AccountDetailService.java
+++ src/main/java/edu/codegym/toam/service/account/AccountDetailService.java
@@ -3,32 +3,20 @@ package edu.codegym.toam.service.account;
 import edu.codegym.toam.model.Account;
 import edu.codegym.toam.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.ArrayList;
 
 @Service
-public class AccountService implements IAccountService {
+public class AccountDetailService implements UserDetailsService {
     @Autowired
     AccountRepository accountRepository;
-
     @Override
-    public Iterable<Account> findAll() {
-        return accountRepository.findAll();
-    }
-
-    @Override
-    public Account findById(Long id) {
-        return accountRepository.findById(id).get();
-    }
-
-    @Override
-    public Account findByUsername(String username) {
-        return accountRepository.findAccountByUsername(username);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("foo", "foo", new ArrayList<>());
     }
 }
