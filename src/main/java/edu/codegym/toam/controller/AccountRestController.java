8425ff1e154b7476e23e8aa3cf1180e32106f39a
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-07-25 14:04:15 +0700

    refactor

--- src/main/java/edu/codegym/toam/controller/AccountRestController.java
+++ src/main/java/edu/codegym/toam/controller/AccountRestController.java
@@ -5,6 +5,7 @@ import edu.codegym.toam.service.account.IAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,23 +16,27 @@ public class AccountRestController {
     IAccountService accountService;
 
     @GetMapping("")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Iterable<Account>> getAccounts() {
         return ResponseEntity.ok(this.accountService.findAll());
     }
 
     //    Lấy danh sách tất cả những thằng chủ nhà
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/host")
     public ResponseEntity<Iterable<Account>> getHost() {
         return ResponseEntity.ok(this.accountService.findAllHost());
     }
 
     //Lấy danh sách tất cả những thằng thuê nhà
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/renter")
     public ResponseEntity<Iterable<Account>> getRenter() {
         return ResponseEntity.ok(this.accountService.findAllRenter());
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Account> getAccountById(@PathVariable Long id) {
         try {
             return ResponseEntity.ok(this.accountService.findById(id));
@@ -41,6 +46,7 @@ public class AccountRestController {
     }
 
     @PostMapping()
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Account> createAccount(@RequestBody Account account) {
 
         try {
@@ -52,6 +58,7 @@ public class AccountRestController {
     }
 
     @PutMapping()
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Account> updateAccount(@RequestBody Account account) {
         try {
             return ResponseEntity.ok(this.accountService.update(account));
@@ -61,6 +68,7 @@ public class AccountRestController {
 
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> removeBlogById(@PathVariable Long id) {
         try {
@@ -70,5 +78,4 @@ public class AccountRestController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-
 }
