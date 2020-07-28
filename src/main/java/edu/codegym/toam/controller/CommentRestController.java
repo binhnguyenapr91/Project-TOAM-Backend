96089731c9ade92b77753bf38fc03d6302a4d252
sonnguyenece@gmail.com <sonnguyenece@gmail.com>
2020-07-28 09:02:06 +0700

    add comments for CommentRestController

--- src/main/java/edu/codegym/toam/controller/CommentRestController.java
+++ src/main/java/edu/codegym/toam/controller/CommentRestController.java
@@ -35,7 +35,6 @@ public class CommentRestController {
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
         }
-
     }
 
     @PutMapping()
@@ -57,11 +56,12 @@ public class CommentRestController {
         }
     }
 
+//    Lấy tất cả comments của 1 property
     @GetMapping("/property/{propertyId}")
     public ResponseEntity<Iterable<Comments>> getPropertyComments(@PathVariable Long propertyId) {
         return ResponseEntity.ok(this.commentsService.findAllCommentByPropertyId(propertyId));
     }
-
+// Tạo comments của 1 property
     @PostMapping("create/property/{propertyId}")
     public ResponseEntity<Iterable<Comments>> createPropertyComments(@PathVariable Long propertyId) {
         return ResponseEntity.ok(this.commentsService.findAllCommentByPropertyId(propertyId));
